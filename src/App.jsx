81d2a528f4372359e8fc37561f446d6716cf1ff1
manthan37 <manthankyada37@gmail.com>
2024-07-20 08:29:14 -0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive");
+  function changeColor(newColor) {
+    setColor(newColor);
+  }
 
   return (
-    <>
-      <h1 className='bg-green-500 text-3xl'>Bg changer</h1>
-    </>
-  )
+    <div
+      className="w-full h-screen duration-200 "
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
+          <button
+            onClick={() => changeColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg bg-red-600"
+          >
+            Red
+          </button>
+          <button
+            onClick={() => changeColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg bg-blue-600"
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => changeColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg bg-green-600"
+          >
+            Green
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
